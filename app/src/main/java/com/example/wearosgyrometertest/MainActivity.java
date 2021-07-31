f19e8e062a5cb17c554c927227012fdfe6ddba0b
lemonywinter <lemonostril@gmail.com>
2021-07-30 22:02:23 -0700

    Initial Commit

---
+++ app/src/main/java/com/example/wearosgyrometertest/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.wearosgyrometertest;
+
+import android.app.Activity;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.example.wearosgyrometertest.databinding.ActivityMainBinding;
+
+public class MainActivity extends Activity implements SensorEventListener {
+    private TextView mTextView;
+    private ActivityMainBinding binding;
+
+    private SensorManager sensorManager;
+    private Sensor sensor;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        mTextView = binding.text;
+    }
+
+    protected void onResume() {
+        super.onResume();
+        sensorManager.registerListener(this, sensor, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    protected void onPause() {
+        super.onPause();
+        sensorManager.unregisterListener(this);
+    }
+
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+    }
+
+    public void onSensorChanged(SensorEvent event) {
+        float x = event.values[0];
+        float y = event.values[1];
+        float z = event.values[2];
+
+        String valString = "x: " + x + "\ny: " + y + "\nz: " + z;
+    }
+}
